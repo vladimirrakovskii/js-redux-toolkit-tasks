57d4620270d2c1e5b2a343005732a950e72dad4f
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:07:52 +0300

    Update commentsSlice.js

--- 7/src/slices/commentsSlice.js
+++ 7/src/slices/commentsSlice.js
@@ -1,5 +1,19 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
+const commentsAdapter = createEntityAdapter()
+const initialState = commentsAdapter.getInitialState();
 
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState: initialState,
+    reducers: {
+        addComment: commentsAdapter.addOne,
+        addComments: commentsAdapter.addMany,
+    }
+})
+
+export const { actions } = commentsSlice;
+export const selectors = commentsAdapter.getSelectors((state) => state.comments);
+export default commentsSlice.reducer;
 // END
