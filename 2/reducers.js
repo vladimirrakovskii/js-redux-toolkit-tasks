76fb37b6177d462f766bb59895e3e8e865fac623
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:51:48 +0300

    Update reducers.js

--- 2/reducers.js
+++ 2/reducers.js
@@ -3,13 +3,33 @@ import { combineReducers } from "redux";
 
 const comments = (state = {}, action) => {
   // BEGIN (write your solution here)
-  
+  switch (action.type) {
+    case 'TASK_COMMENT_ADD':
+      const id = action.payload.comment.id
+      const data = {[id]: action.payload.comment};
+      return {...state, ...data};
+    case 'TASK_COMMENT_REMOVE':
+      return _.omit(state, [action.payload.id]);
+    case 'TASK_REMOVE':
+      return _.omitBy(state, (comment) => comment.taskId === action.payload.id);
+    default:
+        return state;
+  }
   // END
 };
 
 const tasks = (state = {}, action) => {
   // BEGIN (write your solution here)
-
+switch (action.type) {
+    case 'TASK_ADD':
+      const id = action.payload.task.id
+      const data = {[id]: action.payload.task};
+      return {...state, ...data};
+    case 'TASK_REMOVE':
+      return _.omit(state, [action.payload.id]);
+    default:
+      return state;
+  }
   // END
 };
 
