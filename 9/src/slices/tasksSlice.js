7c2f048e97590ae013dd4cf64a69aad463ebd6a2
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:15:44 +0300

    Update tasksSlice.js

--- 9/src/slices/tasksSlice.js
+++ 9/src/slices/tasksSlice.js
@@ -13,5 +13,32 @@ export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
 });
 
 // BEGIN (write your solution here)
+export const createTask = createAsyncThunk("tasks/createTask", async (name) => {
+      const response = await axios.post(routes.tasksPath(), { name });
+      return response.data;
+});
 
+export const removeTask = createAsyncThunk("tasks/removeTask", async (id) => {
+      await axios.delete(routes.taskPath(id));
+      return id;
+});
+
+const tasksAdapter = createEntityAdapter()
+const initialState = tasksAdapter.getInitialState();
+
+const tasksSlice = createSlice({
+  name: "items",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchTasks.fulfilled, tasksAdapter.setAll)
+        .addCase(createTask.fulfilled, tasksAdapter.addOne)
+        .addCase(removeTask.fulfilled, tasksAdapter.removeOne);
+  },
+});
+
+export const { actions } = tasksSlice;
+export const selectors = tasksAdapter.getSelectors((state) => state.tasks);
+export default tasksSlice.reducer;
 // END
