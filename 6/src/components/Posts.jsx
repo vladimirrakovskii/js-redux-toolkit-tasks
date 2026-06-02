77b120b1b7f93300646266a51c883db83cddafad
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:03:41 +0300

    Update Posts.jsx

--- 6/src/components/Posts.jsx
+++ 6/src/components/Posts.jsx
@@ -5,7 +5,14 @@ import Post from './Post.jsx';
 
 const Posts = () => {
   // BEGIN (write your solution here)
-
+    const posts = useSelector((state) => state.postsReducer.entities);
+    return (
+        <div className="mt-3">
+            {Object.values(posts).map((post) => (
+                <Post key={post.id} post={post}/>
+            ))}
+        </div>
+    );
   // END
 };
 
