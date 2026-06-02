04c9343e4152ddba01fa73ed212bb5a3a8f04422
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:53:49 +0300

    Update middlewares.js

--- 3/src/middlewares.js
+++ 3/src/middlewares.js
@@ -9,7 +9,12 @@ const logger = (store) => (next) => (action) => {
 
 const addDate = (store) => (next) => (action) => {
   // BEGIN (write your solution here)
-
+  if (action.type !== "TASK_ADD") {
+    return next(action);
+  }
+  const localDate = new Date().toLocaleDateString('ru-RU');
+  action.payload.task.text = "Задача на " + localDate + ": " + action.payload.task.text;
+  return next(action);
   // END
 };
 
