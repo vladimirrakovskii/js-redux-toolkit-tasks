14d8e4c376af052eaa55461f8f00066fda1493f8
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:19:00 +0300

    Update tasksApi.js

--- 10/src/services/tasksApi.js
+++ 10/src/services/tasksApi.js
@@ -1,5 +1,34 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // BEGIN (write your solution here)
+export const tasksApi = createApi({
+    reducerPath: "tasksApi",
+    baseQuery: fetchBaseQuery({ baseUrl: '/api/tasks' }),
+    endpoints: (builder) => ({
+        getTasks: builder.query({
+            query: () => '',
+            providesTags: ['Tasks'],
+        }),
+        addTask: builder.mutation({
+            query: (text) => ({
+                method: 'POST',
+                body: { text },
+            }),
+            invalidatesTags: ['Tasks'],
+        }),
+        removeTask: builder.mutation({
+            query: (id) => ({
+                url: id,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Tasks'],
+        }),
+    }),
+});
 
+export const {
+    useGetTasksQuery,
+    useAddTaskMutation,
+    useRemoveTaskMutation,
+} = tasksApi;
 // END
