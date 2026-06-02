a15b1bb56b463b0787c4591aacbd7c2a034f9ae1
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:58:15 +0300

    Update Comment.jsx

--- 5/src/components/Comment.jsx
+++ 5/src/components/Comment.jsx
@@ -3,7 +3,14 @@ import { useSelector } from 'react-redux';
 
 const Comment = ({ commentId }) => {
   // BEGIN (write your solution here)
-
+    const comment = useSelector((state) => {
+        const currentComment = state.commentsReducer.comments.find(( comment ) => comment.id === commentId);
+        return currentComment;
+    });
+    const author = useSelector((state) => {
+        const currentAuthor = state.usersReducer.users.find(({ id }) => id === comment.author);
+        return currentAuthor;
+    });
   // END
 
   if (!author || !comment) {
