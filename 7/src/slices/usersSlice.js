36d90d7115f81a2ef8b1037afaa9572614057145
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:09:22 +0300

    Update usersSlice.js

--- 7/src/slices/usersSlice.js
+++ 7/src/slices/usersSlice.js
@@ -1,5 +1,18 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
+const usersAdapter = createEntityAdapter()
+const initialState = usersAdapter.getInitialState();
 
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: initialState,
+    reducers: {
+        addUsers: usersAdapter.addMany,
+    }
+})
+
+export const { actions } = usersSlice;
+export const selectors = usersAdapter.getSelectors((state) => state.users);
+export default usersSlice.reducer;
 // END
