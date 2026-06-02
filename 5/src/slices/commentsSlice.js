14898879f60d6eb5a98904603d37ddfd6b31960b
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:58:53 +0300

    Update commentsSlice.js

--- 5/src/slices/commentsSlice.js
+++ 5/src/slices/commentsSlice.js
@@ -1,3 +1,20 @@
 // BEGIN (write your solution here)
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    comments: [],
+};
+
+const commentsSlice = createSlice({
+    name: "comments",
+    initialState,
+    reducers: {
+        setComments(state, { payload }) {
+            state.comments = payload;
+        },
+    },
+});
+
+export const { actions } = commentsSlice;
+export default commentsSlice.reducer;
 // END
