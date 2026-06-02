543b0ce652eae4f9c257a007e77a91b73d525451
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:08:52 +0300

    Update postsSlice.js

--- 7/src/slices/postsSlice.js
+++ 7/src/slices/postsSlice.js
@@ -1,5 +1,20 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
+const postsAdapter = createEntityAdapter()
+const initialState = postsAdapter.getInitialState();
 
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: initialState,
+    reducers: {
+        addPost: postsAdapter.addOne,
+        addPosts: postsAdapter.addMany,
+        updatePost: postsAdapter.updateOne,
+    }
+})
+
+export const { actions } = postsSlice;
+export const selectors  = postsAdapter.getSelectors((state) => state.posts);
+export default postsSlice.reducer;
 // END
